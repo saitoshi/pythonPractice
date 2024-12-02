7053bb9d40ee8fec0e5488e4a4e0f455f993a487
Shun <saitoshin2145@gmail.com>
2024-12-02 09:02:16 +0900

    2024-12-02-added udemy practice

---
+++ udemy_practice/practice1.py
@@ -0,0 +1,5 @@
+# Display the greeting line 
+print("Welcome to the Band Name Generator\n")
+city = input("What is the city you grew up in?\n")
+petName = input("What is your pet's name?\n")
+print("Your band name could be: " + city + " " + petName)
