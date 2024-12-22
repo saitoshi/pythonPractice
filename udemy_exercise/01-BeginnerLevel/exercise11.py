b734c7c9d850312c1f0ae611a898a6be04dd32ac
Shun <saitoshin2145@gmail.com>
2024-12-23 08:59:54 +0900

    20241223-Python IsPrime

---
+++ udemy_exercise/01-BeginnerLevel/exercise11.py
@@ -0,0 +1,19 @@
+'''
+@name is_prime 
+@desc Determines whether the input is a prime number or not 
+@input num - The number to be determined whether it is prime or not
+@return TRUE or FALSE 
+'''
+def is_prime(num):
+    print(num)
+    isPrime = True 
+    if (num == 0 or num == 1):
+        isPrime = False
+    elif num > 1:
+        for i in range (2, num):
+            print(i)
+            if (num % i == 0):
+                isPrime = False
+                break
+    return isPrime
+print(is_prime(5))
---
+++ udemy_practice/calculator/calculator.py
@@ -0,0 +1,8 @@
+'''
+@file_name: calculator.py
+@desc A file that replicates the calculator 
+@return 
+'''
+
+def add(a,b):
+    return a + b
